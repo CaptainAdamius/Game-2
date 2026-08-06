5d404bb8dd729bcc2c0abcd99a728d9c0625c18c
JackHxo <wdg0128@autuni.ac.nz>
2026-08-06 18:19:35 +1200

    outtro

--- Assets/Jack/Handshake_images/handshaking.cs
+++ Assets/Jack/Handshake_images/handshaking.cs
@@ -3,24 +3,36 @@ using UnityEngine;
 public class handshaking : MonoBehaviour
 {
     private Animator anim;
+    int handShakeIndex;
+    [SerializeField] int handShakeGoal;
+    bool handPos;
 
     void Start()
     {
         anim=GetComponent<Animator>();
+        handPos = true;
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (SceneIntroManager.IsIntroActive) return;
+
+        if (Input.GetKeyDown(KeyCode.W) && handPos)
         {
-        
+            handShakeIndex++;
             anim.SetBool("Hand_down", true);
+            handPos = false;
 
         }
-        else if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKeyDown(KeyCode.S)&& !handPos)
         {
             anim.SetBool("Hand_down", false);
+            handPos = true;
 
+        }
+        if (handShakeIndex >= handShakeGoal)
+        {
+            GameData.GDTaskComplete = true;
         }
     }
 
